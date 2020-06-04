126017b52bbc9522fd9b41ef81cb820b70e4b5df
Joshua Brook <joshuabrookuk@gmail.com>
2020-06-04 17:24:37 +0100

    /bookmarks displays hyperlink as title.

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -1,11 +1,12 @@
 require 'sinatra'
 
 feature "/add-bookmark" do
-  xscenario "adds a bookmark" do
+  scenario "adds a bookmark" do
     visit "/add-bookmark"
     fill_in 'url', with: 'thisisatest.com'
+    fill_in 'title', with: 'bookmark_title'
     click_on 'Submit'
     expect(page).to have_content('thisisatest.com')
+    expect(page).to have_content('bookmark_title')
   end
 end
-
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 
 feature "/bookmarks page" do
-  xscenario "views bookmarks" do
-    add_test_data
+  scenario "views bookmarks" do
+    Bookmark.create('http://www.google.com','Google')
+    Bookmark.create('http://www.makersacademy.com','Makers Academy')
+    Bookmark.create('http://www.destroyallsoftware.com','Destory All Software')
     visit '/bookmarks'
     expect(page).to have_content("http://www.google.com")
     expect(page).to have_content("http://www.makersacademy.com")
     expect(page).to have_content("http://www.destroyallsoftware.com")
+    expect(page).to have_content("Google")
+    expect(page).to have_content("Makers Academy")
+    expect(page).to have_content("Destory All Software")
   end
 end
-
