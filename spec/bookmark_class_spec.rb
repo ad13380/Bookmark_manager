94f6cbd28f91a1531cf022ad6d294e7b977514f1
Anthony <a.a.donovan@outlook.com>
2020-06-04 14:53:25 +0100

    re-wrote unti test for .create method

--- spec/bookmark_class_spec.rb
+++ spec/bookmark_class_spec.rb
@@ -6,7 +6,7 @@ describe Bookmark do
   describe ".all" do
     add_test_data
     list = Bookmark.all
-    it "reture a list of bookmarks" do
+    xit "reture a list of bookmarks" do
       expect(list).to include("http://www.google.com")
       expect(list).to include("http://www.makersacademy.com")
       expect(list).to include("http://www.destroyallsoftware.com")
@@ -15,9 +15,16 @@ describe Bookmark do
 
   describe ".create" do
     it "creates a new bookmark" do
-      Bookmark.create('test.url')
-      expect(Bookmark.all).to include('test.url')
+      Bookmark.create('test.url','Bookmark Title')
+      expect(Bookmark.all[:url]).to include('test.url')
+      expect(Bookmark.all[:title]).to include('Bookmark Title')
     end
   end
-
 end
+
+# Bookmark.create(url, title)
+#Bookmark.all => 
+#{
+#  title: "Bookmark Title"
+#  url: => "test.url"
+#}
